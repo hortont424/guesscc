2fbd9f31b8b0d1e9eccb6fa6aeffef99407466f0
Tim Horton <hortont424@gmail.com>
2010-05-16 00:26:01 -0400

    Symbol table generator more or less works

--- generateSymbolTable.py
+++ generateSymbolTable.py
@@ -2,14 +2,57 @@
 
 # nm -g -U -j -f -A -m /usr/lib/libssl.dylib
 
+import os
+import magic
 from subprocess import Popen, PIPE
 from collections import defaultdict
 
-def list_library_symbols(libraryName):
-    args = ["/usr/bin/nm", "-g", "-U", "-j", "-f", "-A", "-m", libraryName]
-    symbols = Popen(args, stdout=PIPE).communicate()[0].splitlines()
+ms = magic.open(magic.MAGIC_NONE)
+ms.load()
+
+def list_library_symbols(filename):
+    args = ["/usr/bin/nm", "-g", "-U", "-j", "-f", "-A", "-m", filename]
+    symbols = Popen(args, stdout=PIPE, stderr=PIPE).communicate()[0].splitlines()
     symbols = [sym.split(" ") for sym in symbols]
     symbols = [sym[4] for sym in symbols if sym[3] == "external"]
+
     return symbols
 
-print list_library_symbols("/usr/lib/libssl.dylib")
+def libname_from_filename(filename):
+    libname = os.path.basename(filename).split("-")[0].split(".")[0]
+
+    if libname.startswith("lib"):
+        libname = libname[3:]
+
+    return libname
+
+def file_is_library(filename):
+    # TODO: adjust to accept elf, etc.
+    if ms.file(filename).find("Mach-O") >= 0:
+        return True
+
+    return False
+
+def generate_symbol_table(libpaths):
+    symbolTable = defaultdict(set)
+
+    for libpath in libpaths:
+        for (root, subdirs, files) in os.walk(libpath):
+            for file in files:
+                filename = os.path.join(root, file)
+
+                if os.path.islink(filename):
+                    continue
+
+                if not file_is_library(filename):
+                    continue
+
+                libname = libname_from_filename(filename)
+                symbols = list_library_symbols(filename)
+
+                for symbol in symbols:
+                    symbolTable[symbol].add(libname)
+
+    return symbolTable
+
+print generate_symbol_table(["/usr/lib", "/usr/local/lib"])
